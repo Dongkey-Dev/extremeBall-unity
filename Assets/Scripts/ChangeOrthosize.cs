b733cb4c2ac993f48c8a4046c3e925f2b9715e77
Dongkey-Dev <dongkeydev@gmail.com>
2022-02-22 01:54:45 +0900

    modify func : stop change camera orthosize when mouse clicking

--- Assets/Scripts/ChangeOrthosize.cs
+++ Assets/Scripts/ChangeOrthosize.cs
@@ -12,12 +12,14 @@ public class ChangeOrthosize : MonoBehaviour
         vcam.m_Lens.OrthographicSize = 8;
     }    
     void FixedUpdate(){
-        PlayerMagnitude = ShootToMouse.PlayerVelocity.magnitude;
-        if(PlayerMagnitude > 12){
-            vcam.m_Lens.OrthographicSize = Mathf.Max(8, Mathf.Min(vcam.m_Lens.OrthographicSize+0.2f, 20));
-        }
-        else{
-            vcam.m_Lens.OrthographicSize = Mathf.Max(8, Mathf.Min(vcam.m_Lens.OrthographicSize-0.1f, 20));
+        if (!Input.GetMouseButton(0)){
+            PlayerMagnitude = ShootToMouse.PlayerVelocity.magnitude;
+            if(PlayerMagnitude > 12){
+                vcam.m_Lens.OrthographicSize = Mathf.Max(8, Mathf.Min(vcam.m_Lens.OrthographicSize+0.2f, 20));
+            }
+            else{
+                vcam.m_Lens.OrthographicSize = Mathf.Max(8, Mathf.Min(vcam.m_Lens.OrthographicSize-0.1f, 20));
+            }
         }
     }
 }
