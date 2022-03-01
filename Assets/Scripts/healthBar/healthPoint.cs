713fa1c656b187c585c2b28d6eacbe7c0f48a79a
Dongkey-Dev <dongkeydev@gmail.com>
2022-03-01 23:03:45 +0900

    feat : add HP on playerball

--- Assets/Scripts/healthBar/healthPoint.cs
+++ Assets/Scripts/healthBar/healthPoint.cs
@@ -6,12 +6,13 @@ using PlayerMoudle;
 public class healthPoint : MonoBehaviour
 {
     public static RectTransform rectTransform;
-
     void Start(){
         rectTransform = GetComponent<RectTransform>();
     }
-    void Update(){
-        if (Input.GetMouseButton(0)){
+    void FixedUpdate(){
+        if(rectTransform.sizeDelta.x <= 0.0f){
+        } 
+        if(Input.GetMouseButton(0)){
             rectTransform.sizeDelta -= new Vector2(0.5f,0);
         }
         else{
