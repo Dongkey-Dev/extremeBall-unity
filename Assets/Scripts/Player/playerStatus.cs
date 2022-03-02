ebfdd354fe15b17c1906cb9ea77dd857cf76c5fc
Dongkey-Dev <dongkeydev@gmail.com>
2022-03-03 01:29:14 +0900

    feat : change player position when it be end of map

---
+++ Assets/Scripts/Player/playerStatus.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStatus : MonoBehaviour
+{
+    public static float maxHP;
+    public static float currentHP;
+    // Start is called before the first frame update
+    void Start(){
+        maxHP = 200.0f;
+        currentHP = 200.0f;
+    }
+    void FixedUpdate(){
+        if(this.transform.position.x >= 1050 ){
+            this.transform.position = new Vector3(-1050,this.transform.position.y, this.transform.position.z);
+            // ChangeOrthosize.vcam.transform.position = new Vector3(-1050,this.transform.position.y, this.transform.position.z);
+        } else if(this.transform.position.x <= -1050 ){
+            this.transform.position = new Vector3(1050,this.transform.position.y, this.transform.position.z);
+            // ChangeOrthosize.vcam.transform.position = new Vector3(1050,this.transform.position.y, this.transform.position.z);
+        }
+        if(currentHP <= 0.0f){
+            Destroy(this.gameObject);
+        } 
+        if(Input.GetMouseButton(0)){
+            currentHP -= 1.0f;
+        }
+        else{
+            currentHP -= 0.2f;
+        }
+    }
+}
