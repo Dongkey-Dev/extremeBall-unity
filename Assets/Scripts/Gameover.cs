2f6bb356983a3bf2a6c202d0391c4302fe7652b8
Dongkey-Dev <dongkeydev@gmail.com>
2022-03-09 01:58:56 +0900

    feat : Add restart when press any key in game over screen

--- Assets/Scripts/Gameover.cs
+++ Assets/Scripts/Gameover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gameover : MonoBehaviour
 {
@@ -9,5 +10,14 @@ public class Gameover : MonoBehaviour
     public void Setup(int score){
         gameObject.SetActive(true);
         pointsText.text = "Point : " + score.ToString();
+    }
+    public void Restart(){
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void Update(){
+        if(Input.anyKey){
+            Restart();
+        }
     }
 }
