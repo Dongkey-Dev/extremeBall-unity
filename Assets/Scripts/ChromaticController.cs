d17b5a81489ddb024a7c7105621ab4cab9912d10
Dongkey-Dev <dongkeydev@gmail.com>
2022-03-14 02:29:43 +0900

    feat : Add chromatic aberration controller to controll intensity with script

---
+++ Assets/Scripts/ChromaticController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChromaticController : MonoBehaviour
+{
+    ChromaticAberration chromatic;
+    PostProcessVolume volumeChromatic;
+    void Start()
+    {
+        chromatic = ScriptableObject.CreateInstance<ChromaticAberration>();
+        chromatic.enabled.Override(true);
+        chromatic.intensity.Override(.35f);
+        volumeChromatic = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, chromatic);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0)){
+            chromatic.intensity.value = .75f;
+        }
+        else{
+            chromatic.intensity.value = .35f;
+        }
+    }
+}
