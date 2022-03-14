27b3cf35778b6978f62637d60d8c2016ad8cac28
Dongkey-Dev <dongkeydev@gmail.com>
2022-03-15 02:10:00 +0900

    feat : Add gradient color score ball effect

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -18,7 +18,7 @@ public class PlayerCollision : MonoBehaviour
     {
         ScoreNum = 0; 
         MyscoreText.text = "Score : " + ScoreNum;
-        ps_main = ps.main;
+        ps_main = ps.main;  
     }
 
     private void OnTriggerEnter2D(Collider2D score)
@@ -26,7 +26,7 @@ public class PlayerCollision : MonoBehaviour
         if(score.tag == "yellow_score"){
             ScoreNum += 100;
             MyscoreText.text = "Score : " + ScoreNum;
-            ExplodeScore(score.transform.position, score.transform.rotation);
+            ExplodeScore("yellow",score.transform.position, score.transform.rotation);
             Destroy(score.gameObject);
             playerStatus.currentHP = Mathf.Min(playerStatus.currentHP+50, playerStatus.maxHP);
             SoundManagerScript.playSound("scoreBallSound");
@@ -34,20 +34,71 @@ public class PlayerCollision : MonoBehaviour
         else if(score.tag == "orange_score"){
             ScoreNum += 300;
             MyscoreText.text = "Score : " + ScoreNum;
-            ExplodeScore(score.transform.position, score.transform.rotation);
+            ExplodeScore("orange", score.transform.position, score.transform.rotation);
             Destroy(score.gameObject);            
             playerStatus.currentHP = playerStatus.maxHP;
             SoundManagerScript.playSound("scoreBallSound");
         }        
+        else if(score.tag == "purple_score"){
+            ScoreNum += 300;
+            MyscoreText.text = "Score : " + ScoreNum;
+            ExplodeScore("purple",score.transform.position, score.transform.rotation);
+            Destroy(score.gameObject);            
+            playerStatus.currentHP = playerStatus.maxHP;
+            SoundManagerScript.playSound("scoreBallSound");
+        }                
         else if(score.tag == "red_ball" | score.tag == "Lava"){
             playerStatus.currentHP = 0.0f;
         }
         CameraShake.Instance.ShakeCamera(10f, .2f);
     }
 
-    void ExplodeScore(Vector3 pos, Quaternion rot)
+    public Gradient getGradient(Color c1, Color c2)
+    {
+        Gradient gradient = new Gradient();
+ 
+        GradientColorKey[] gradientColors = new GradientColorKey[2];
+        gradientColors[0].color = c1;
+        gradientColors[1].color = c2;
+        gradientColors[0].time = 0f;
+        gradientColors[1].time = 1f;
+ 
+        GradientAlphaKey[] gradientAlpha = new GradientAlphaKey[2];
+        gradientAlpha[0].alpha = 1;
+        gradientAlpha[0].time = 0;
+        gradientAlpha[1].alpha = 1;
+        gradientAlpha[1].time = 1f ;
+ 
+        gradient.SetKeys(gradientColors, gradientAlpha);
+        return gradient;
+    }    
+
+    void ExplodeScore(string color, Vector3 pos, Quaternion rot)
     {
         ps_main.startSpeed = ShootToMouse.PlayerVelocity.magnitude;
+        switch(color)
+        {
+            case "yellow":      
+                ps_main.startColor = new ParticleSystem.MinMaxGradient(
+                    new Vector4(1,(float)246/255,(float)187/255,1), 
+                    new Vector4((float)255/255,(float)194/255,0,1)
+                );
+                break;
+
+            case "orange":
+                ps_main.startColor = new ParticleSystem.MinMaxGradient(
+                    new Vector4(1,(float)108/255,(float)61/255,1), 
+                    new Vector4((float)229/255,(float)121/255,0,1)
+                );
+                break;
+
+            case "purple":
+                ps_main.startColor = new ParticleSystem.MinMaxGradient(
+                    new Vector4(1,(float)0/255,(float)124/255,1), 
+                    new Vector4((float)123/255,(float)255/255,1,1)
+                );
+                break;
+        }
         Instantiate(ps, pos, rot);
     }
 }
